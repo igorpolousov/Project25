65536c3eb96504ca0fd89167bb5fb00ff6102818
Igor Polousov <igorpolousov@gmail.com>
2022-02-24 13:23:34 +0300

    + comments

--- Project25/ViewController.swift
+++ Project25/ViewController.swift
@@ -6,27 +6,38 @@
 //
 
 import UIKit
+// Поддержка однорангового подключения peer to peer и обнаружение близлежащих устройств
 import MultipeerConnectivity
 
 class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
 
+    
+    // Массив с объектами UIImage
     var images = [UIImage]()
     
+    // Peer ID
     var peerID = MCPeerID(displayName: UIDevice.current.name)
+    // Объект включает и управляет соединениями
     var mcSession: MCSession?
+    // Позволяет польлзователю приложения принимать уведомления и делать ответы на уведомления
     var mcAdvertiserAssistant: MCAdvertiserAssistant?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Добавление названия приложения, кнопки добавить сессию и кнопки выбрать картинку
         title = "Selfie share"
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
         
+        // Создание сессии и передача управления сессией
         mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
         mcSession?.delegate = self
     }
 
+    
+    // MARK: Collection view data source
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
@@ -41,12 +52,14 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         return cell
     }
     
+    // Начало соединения
     func startHosting(action: UIAlertAction) {
         guard let mcSession = mcSession else { return }
-        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-prohect25", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-project25", discoveryInfo: nil, session: mcSession)
         mcAdvertiserAssistant?.start()
     }
     
+    // Присоединение к существующей сессии
     func joinSession(action: UIAlertAction) {
         guard let mcSession = mcSession else { return }
         let mcBrowser = MCBrowserViewController(serviceType: "hws-project25", session: mcSession)
@@ -54,6 +67,7 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         present(mcBrowser, animated: true)
     }
     
+    // Окно с опциями для выбора действий при нажатии кнопки добавить
     @objc func showConnectionPrompt() {
         let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
@@ -62,6 +76,7 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         present(ac, animated: true)
     }
     
+    // Метод для выбора картинки из медиатеки
     @objc func importPicture() {
         let picker = UIImagePickerController()
         picker.allowsEditing = true
@@ -70,16 +85,15 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Проверка на наличие картинки
         guard let image = info[.editedImage] as? UIImage else { return }
         dismiss(animated: true, completion: nil)
         
         images.insert(image, at: 0)
         collectionView.reloadData()
         
-        guard let mcSession = mcSession else {
-            return
-        }
-
+        guard let mcSession = mcSession else { return }
+        
         if mcSession.connectedPeers.count > 0 {
             if let imageData = image.pngData() {
                 do {
@@ -94,15 +108,15 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     }
     
     func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-        <#code#>
+        
     }
     
     func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
-        <#code#>
+        
     }
     
     func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
-        <#code#>
+        
     }
     
     func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
